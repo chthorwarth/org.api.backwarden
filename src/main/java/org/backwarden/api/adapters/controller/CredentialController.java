5ab2defac1d32390075649d678d811b3d95c2573
Christian Thorwarth <christian.thorwarth@study.thws.de>
2025-12-13 16:27:44 +0100

    embedded /credentials into openapi

--- src/main/java/org/backwarden/api/adapters/controller/CredentialController.java
+++ src/main/java/org/backwarden/api/adapters/controller/CredentialController.java
@@ -9,35 +9,31 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import org.backwarden.api.adapters.controller.model.CredentialDTO;
-import org.backwarden.api.adapters.controller.model.interfaces.CredentialsApi;
 import org.backwarden.api.logic.model.Credential;
 import org.backwarden.api.logic.ports.input.CredentialAPI;
 import org.backwarden.api.logic.services.CredentialService;
+import org.openapitools.api.CredentialsApi;
+import org.openapitools.model.CredentialDTO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
-public class CredentialController implements CredentialsApi
-{
+@Path("/")
+public class CredentialController implements CredentialsApi {
 
-    CredentialAPI credentialAPI;
-
-	@Override
-    public List<CredentialDTO> vaultsVaultIdCredentialsGet(@PathParam("vaultId") Integer vaultId)
-    {
+    @Override
+    public List<CredentialDTO> vaultsVaultIdCredentialsGet(Integer vaultId) {
         List<CredentialDTO> credentialDTOs = new ArrayList<>();
         CredentialDTO credentialDTO = new CredentialDTO();
-        credentialDTO.setId(1);
+        credentialDTO.id(1L);
         credentialDTO.setTitle("Credential");
         credentialDTOs.add(credentialDTO);
         return credentialDTOs;
-	}
+    }
 
     @Override
-    public void vaultsVaultIdCredentialsPost(@PathParam("vaultId") Integer vaultId,@Valid @NotNull CredentialDTO credentialDTO)
-    {
+    public void vaultsVaultIdCredentialsPost(Integer vaultId, org.openapitools.model.CredentialDTO credentialDTO) {
         System.out.printf(credentialDTO.getTitle());
     }
 
--- src/main/java/org/backwarden/api/adapters/controller/model/interfaces/CredentialsApi.java
+++ src/main/java/org/backwarden/api/adapters/controller/model/interfaces/CredentialsApi.java
@@ -11,6 +11,7 @@ import jakarta.ws.rs.core.Response;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.List;
+
 import jakarta.validation.constraints.*;
 import jakarta.validation.Valid;
 
@@ -19,31 +20,26 @@ import jakarta.validation.Valid;
  */
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2025-12-13T15:14:28.040174+01:00[Europe/Berlin]", comments = "Generator version: 7.17.0")
-@Path("/")
 public interface CredentialsApi {
 
     /**
-     *
-     *
      * @param vaultId
      * @return OK
      */
     @GET
     @Path("/vaults/{vaultId}/credentials")
-    @Produces({ "application/json" })
+    @Produces({"application/json"})
     List<CredentialDTO> vaultsVaultIdCredentialsGet(@PathParam("vaultId") Integer vaultId);
 
 
     /**
-     *
-     *
      * @param vaultId
      * @param vaultsVaultIdCredentialsPostRequest
      * @return Created
      */
     @POST
     @Path("/vaults/{vaultId}/credentials")
-    @Consumes({ "application/json" })
-    void vaultsVaultIdCredentialsPost(@PathParam("vaultId") Integer vaultId,@Valid @NotNull CredentialDTO vaultsVaultIdCredentialsPostRequest);
+    @Consumes({"application/json"})
+    void vaultsVaultIdCredentialsPost(@PathParam("vaultId") Integer vaultId, @Valid @NotNull CredentialDTO vaultsVaultIdCredentialsPostRequest);
 
 }
